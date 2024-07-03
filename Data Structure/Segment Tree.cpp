f847d70ec554a44f71efc7d5000bc92971ccba48
Ismail Akram <76774273+IsmailAkram2000@users.noreply.github.com>
2024-07-04 00:21:02 +0300

    Update Segment Tree.cpp

--- Data Structure/Segment Tree.cpp
+++ Data Structure/Segment Tree.cpp
@@ -2,74 +2,43 @@
 using namespace std;
 #define ll long long
  
-struct segment_tree{
- 
-	int sz;
-	vector<int> v;
- 
-	void init(int n) {
-		sz = 1;
-		while(sz < n)
-			sz *= 2;
-		v.resize(2 * sz);
-	}
- 
-	void set(int i, int x) {
-		set(i, x, 1, 1, sz);
-	}
- 
-	void set(int i, int x, int cur, int l, int r) {
-		if (l == r) {
-			v[cur] = x;
-			return;
-		}
-		int mid = (l + r) / 2;
-		if (i <= mid) 
-			set(i, x, 2 * cur, l, mid);
-		else 
-			set(i, x, 2 * cur + 1, mid + 1, r);
-		v[cur] = v[2 * cur] + v[2 * cur + 1];
-	}  
- 
-	ll query(int l, int r) {
-		return query(l, r, 1, 1, sz); 
-	}
- 
-	ll query(int l, int r, int cur, int lx, int rx) {
-		 if(lx > r || rx < l)	
-			 return 0;
-		 if(lx >= l && rx <= r) 
-			 return v[cur];
-		 int mid = (lx + rx) / 2;
-		 ll s1 = query(l, r, 2 * cur, lx, mid);
-		 ll s2 = query(l, r, 2 * cur + 1, mid + 1, rx);
-		 return s1 + s2;
-	}
- 
+struct segment_tree {
+    int sz;
+    vector<ll> v;
+
+    void init(int n) {
+        sz = 1;
+        while (sz < n)
+            sz *= 2;
+        v.assign(2 * sz, 0);  // Use assign instead of resize and initialize with 0
+    }
+
+    void set(int i, int x) {
+        i += sz - 1;  // Move to the leaf
+        v[i] = x;
+        while (i > 1) {
+            i /= 2;
+            v[i] = max(v[2 * i], v[2 * i + 1]);
+        }
+    }
+
+    ll query(int l, int r) {
+        l += sz - 1;  // Move to the leaf
+        r += sz - 1;
+        ll res = 0;
+        while (l <= r) {
+            if (l % 2 == 1) res = max(res, v[l++]);
+            if (r % 2 == 0) res = max(res, v[r--]);
+            l /= 2;
+            r /= 2;
+        }
+        return res;
+    }
 }seg;
  
 int main(){
  
 	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-	int n, q, x, op;
-	cin >> n >> q;
-	seg.init(n);
-	for(int i = 1; i <= n; i++) {
-		cin >> x;
-		seg.set(i, x);
-	}
-	while(q--) {
-		cin >> op;
-		if (op == 1) {
-			int i, a;
-			cin >> i >> a;
-			seg.set(++i, a);
-		}else {
-			int l, r;
-			cin >> l >> r;
-			cout << seg.sum(l + 1, r) << "\n";
-		}
-	}
  
     return 0;
 }
