bb55bb25084627ebed9b9910b69e6646e2954269
Ismail Akram <76774273+IsmailAkram2000@users.noreply.github.com>
2024-07-13 17:38:28 +0300

    Update Heavy Light Decomposition.cpp

--- Data Structure/Heavy Light Decomposition.cpp
+++ Data Structure/Heavy Light Decomposition.cpp
@@ -37,85 +37,86 @@ struct segment_tree {
 }seg;
 
 struct HLD {
-    const int VAL_ON_EDGES = 1; // make 1 if values on edges otherwise 0
-    const vector<vector<int>> &adj;
-    vector<int> dep, par, root, pos, cntChildren, chld;
+    const int VAL_ON_EDGES = 0; // make 1 if values on edges otherwise 0
+    const vector<vector<int>>& adj;
+    vector<int> dep, par, root, pos, node, cnt_children, chld;
     int nxt_pos;
 
-    HLD(int n, const vector<vector<int>> &G, int treeRoot = 1) : adj(G) {
+    HLD(int n, const vector<vector<int>>& G, int tree_root = 1) : adj(G) {
         nxt_pos = 1;
-        dep.resize(n + 5);
-        par.resize(n + 5);
-        root.resize(n + 5);
-        pos.resize(n + 5);
-        cntChildren.resize(n + 5);
-        chld.resize(n + 5);
-        init(treeRoot);
-        build(treeRoot);
+        dep.resize(n + 1);
+        par.resize(n + 1);
+        root.resize(n + 1);
+        pos.resize(n + 1);
+        node.resize(n + 1);
+        cnt_children.resize(n + 1);
+        chld.resize(n + 1);
+        dfs(tree_root);
+        build(tree_root);
     }
 
-    void init(int v, int p = -1, int d = 0) {
+    void dfs(int v, int p = -1, int d = 0) {
         dep[v] = d;
         par[v] = p;
-        cntChildren[v] = 1;
-        for (int u : adj[v]) {
+        cnt_children[v] = 1;
+        for (auto u : adj[v]) {
             if (u != p) {
-                init(u, v, d + 1);
-                cntChildren[v] += cntChildren[u];
-                if (cntChildren[u] > cntChildren[chld[v]])
+                dfs(u, v, d + 1);
+                cnt_children[v] += cnt_children[u];
+                if (cnt_children[u] > cnt_children[chld[v]])
                     chld[v] = u;
             }
         }
     }
 
-    void build(int v, bool newChain = true) {
-        root[v] = newChain ? v : root[par[v]];
+    void build(int v, bool new_chain = true) {
+        root[v] = new_chain ? v : root[par[v]];
         pos[v] = nxt_pos++;
+        node[nxt_pos - 1] = v;
         if (chld[v])
             build(chld[v], false);
-        for (int u : adj[v]) {
+        for (auto u : adj[v]) {
             if (u != chld[v] && u != par[v])
                 build(u, true);
         }
     }
 
-    inline pair<int, int> moveUp(int &v) { // move up the chain and also change to next position
-        pair<int, int> ans = {pos[root[v]], pos[v]};
+    inline pair<int, int> move_up(int& v) { // move up the chain and also change to next position
+        pair<int, int> ans = { pos[root[v]], pos[v] };
         v = par[root[v]];
         return ans;
     }
 
-    inline void makeULower(int &u, int &v) {
+    inline void make_lower(int& u, int& v) {
         if (dep[root[u]] < dep[root[v]] || (root[u] == root[v] && dep[u] < dep[v])) {
             swap(u, v);
         }
     }
 
-    vector<pair<int, int>> queryPath(int u, int v) {
-        // return all ranges to query in segment tree
+    vector<pair<int, int>> query_path(int u, int v) {
         vector<pair<int, int>> ans;
         while (root[u] != root[v]) {
-            makeULower(u, v);
-            ans.push_back(moveUp(u));
+            make_lower(u, v);
+            ans.push_back(move_up(u));
         }
         // add range between u, v
-        makeULower(u, v);
+        make_lower(u, v);
         if (!VAL_ON_EDGES) // value on nodes
-            ans.push_back({pos[v], pos[u]});
+            ans.push_back({ pos[v], pos[u] });
         else if (u != v) // don't include the root node.
-            ans.push_back({pos[v] + 1, pos[u]});
+            ans.push_back({ pos[v] + 1, pos[u] });
         return ans;
     }
 
-    int get_node(int cur) {
-        return node[cur];
+    pair<int, int> query_subtree(int v) {
+        return { pos[v], pos[v] + cnt_children[v] - 1};
     }
 };
 
 int main() {
-
+    
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-
+	
 
     return 0;
 }
